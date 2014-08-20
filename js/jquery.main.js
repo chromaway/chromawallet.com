b4bc2a651a47c7140d18c8825a3c6c3f180e5670
Петрунин Николай <petruninnikolay@gmail.com>
2014-08-20 09:58:55 +0300

    start

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,3 +1,183 @@
 $(function(){
-
+    var loader = new Loader();
+    var page = new Page();
 } );
+var Page = function(){
+    this.items = $('.site__page');
+    this.window = $(window);
+    this.minHeight = 600;
+    this.contents = $('.site__center');
+    this.timer = setTimeout(function(){}, 0);
+    this.timer2 = setTimeout(function(){}, 0);
+    this.scroll = false;
+    this.scrollMouse = false;
+
+    this.init();
+};
+    Page.prototype = {
+        init: function(){
+            var self = this;
+
+            self.core = self.core();
+            self.core.build();
+        },
+        core: function(){
+            var self = this;
+
+            return {
+                build: function(){
+                    self.core.setSize();
+                    self.core.controls();
+                },
+                setSize: function (){
+                    self.contents.each(function(){
+                        var curItem = $( this );
+
+                        curItem.height( '100%' );
+
+                        var newH = curItem.parents( '.site__page' ).height();
+
+                        if ( newH < self.minHeight ){
+                            newH=self.minHeight;
+                        }
+
+                        curItem.height( newH );
+                    })
+                },
+                controls: function(){
+                    self.window.on({
+                        'resize':function(){
+                            self.core.setSize();
+                        },
+                        'DOMMouseScroll':function( e ){
+                            clearTimeout(self.timer2);
+
+                            self.scrollMouse = true;
+
+                            self.timer2=setTimeout(function(){
+
+                                if( !self.scroll ) {
+                                    var direction = ( e.originalEvent.detail > 0 ) ? -1 : 1;
+                                    if(direction==1){
+                                        self.core.prevPage();
+                                    }else{
+                                        self.core.nextPage();
+                                    }
+                                }
+
+                                self.scroll = false;
+                            },50);
+
+                        },
+                        'mousewheel': function( e ){
+
+                            clearTimeout(self.timer2);
+
+                            self.scrollMouse = true;
+
+                            self.timer2=setTimeout(function(){
+
+                                if( !self.scroll ) {
+                                    var direction = ( e.originalEvent.wheelDelta > 0 ) ? 1 : -1;
+                                    if(direction<0){
+                                        self.core.nextPage();
+                                    }else{
+                                        self.core.prevPage() ;
+                                    }
+                                }
+
+                                self.scroll = false;
+                            },50);
+
+
+                        }
+                    });
+
+                    self.items.on({
+                        'scroll':function(){
+                            if( self.scrollMouse ) {
+                                self.scroll = true;
+                            }
+                            self.scrollMouse = false;
+                            clearTimeout(self.timer2);
+                        }
+                    });
+                },
+                nextPage:function(){
+                    self.timer=setTimeout(function(){
+
+                    },50)
+                },
+                prevPage:function(){
+                    self.timer=setTimeout(function(){
+
+                    },50)
+                }
+            };
+        }
+    };
+
+var Loader = function(){
+    this.obj = $( '.loader' );
+    this.window = $( window );
+    this.minDesktopWidth = 1000;
+    this.percent = { curValue:0 };
+    this.loaded = false;
+    this.firstStep = false;
+    this.desktop = false;
+    this.items = $( '.mobile-desktop' );
+
+    this.init();
+};
+    Loader.prototype = {
+        init: function(){
+            var self = this;
+
+            self.core = self.core();
+            self.core.build();
+        },
+        core: function(){
+            var self = this;
+
+            return {
+                build: function(){
+                    self.core.checkForDesktop();
+                    self.core.checkFirstStep();
+                },
+                checkForDesktop: function(){
+                    console.log( self.window.width(), self.minDesktopWidth );
+                    if( self.window.width() >= self.minDesktopWidth ) {
+
+                        self.items.each( function(){
+                            var curItem = $( this );
+
+                            if( curItem.is( 'img' ) ){
+                                curItem[0].src = curItem[0].src.replace( 'mobile', 'desktop' )
+                            } else {
+
+                            }
+
+                        } );
+                    } else {
+
+                    }
+                },
+                checkFirstStep: function(){
+                    if( self.firstStep && self.loaded  && self.desktop ){
+
+                    } else {
+                        setTimeout( function(){
+                            self.core.checkFirstStep();
+                        }, 1000 );
+                    }
+                },
+                controls: function(){
+                    self.window.on( {
+                        'load': function(){
+
+                        }
+                    } );
+                }
+            };
+        }
+    };
